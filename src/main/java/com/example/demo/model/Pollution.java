447de67d56a3b2670a5fa91b62df101946755e14
Максим <mgfedorenko@yandex.ru>
2023-01-18 21:32:07 +0300

    1

---
+++ src/main/java/com/example/demo/model/Pollution.java
@@ -0,0 +1,26 @@
+package com.example.demo.model;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity(name = "pollution")
+public class Pollution {
+
+
+
+    @Id
+    private String id;
+    private double co;
+    private double no;
+    private double no2;
+    private double o3;
+    private double so2;
+    private double pm2_5;
+    private double pm10;
+    private double nh3;
+
+
+
+}
