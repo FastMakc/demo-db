7f1816aaf1368c6f99fa5d4bce27a1b18a5c8caf
Max Fedorenko <122231638+FastMakc@users.noreply.github.com>
2023-05-23 20:35:20 +0300

    Update PollutionRepository.java

--- src/main/java/com/example/demo/repositories/PollutionRepository.java
+++ src/main/java/com/example/demo/repositories/PollutionRepository.java
@@ -4,5 +4,4 @@ import com.example.demo.model.Pollution;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PollutionRepository extends CrudRepository<Pollution, String> {
-
 }
